f38bae6155f2a52fca583e78673eed2e47416e6a
Richard See <richard@Richards-MacBook-Pro.local>
2018-11-18 16:28:06 +0000

    Put in links in Navbar

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,15 +1,25 @@
 import Link from 'next/link'
+import NavStyles from './styles/NavStyles'
 
 const Nav = () => (
-  <div>
+  <NavStyles>
     {/* Next.js Link - Renders a standard <a/> tag but is bound to HTML5 push state */}
-    <Link href='sell'>
-      <a>Sell!</a>
+    <Link href='/items'>
+      <a>Items</a>
     </Link>
-    <Link href='/'>
-      <a>Home!</a>
+    <Link href='/sell'>
+      <a>Sell</a>
     </Link>
-  </div>
+    <Link href='/signup'>
+      <a>Signup</a>
+    </Link>
+    <Link href='/orders'>
+      <a>Orders</a>
+    </Link>
+    <Link href='/me'>
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 )
 
 export default Nav
